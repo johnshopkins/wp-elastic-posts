d61f47b2143b0a64e4e50abb7f0862ddac10649a
Jen Wachter <jenleighkelly@gmail.com>
2014-04-03 11:13:05 -0400

    Instead of grabbing the post data from WordPress, we query the API, where the data has already been normalized.

--- src/ElasticPosts/Elasticsearch.php
+++ src/ElasticPosts/Elasticsearch.php
@@ -6,22 +6,43 @@ use Secrets\Secret;
 
 class Elasticsearch
 {
+	/**
+	 * WordPress wrapper
+	 * @var object
+	 */
+	protected $wordpress;
+
 	protected $client;
+	protected $httpEngine;
+	protected $apiBase;
 	protected $post_types;
-	protected $wputils;
 
 	public function __construct()
 	{
-		$this->post_types = array_keys(get_option("elastic-posts_post_types_post_types"));
-		$this->index = get_option("elastic-posts_index_index");
-		
+		// allow for dependency injection (testing)
+        $args = func_get_args();
+        $args = array_shift($args);
+
+        $this->wordpress = isset($args["wordpress"]) ? $args["wordpress"] : new \WPUtilities\WordPressWrapper();
+        $this->httpEngine = isset($args["httpEngine"]) ? $args["httpEngine"] : new \HttpExchange\Adapters\Resty(new \Resty());
+        $this->client = isset($args["elasticsearch"]) ? $args["elasticsearch"] : new \Elasticsearch\Client($this->getConfig());
+
+        $this->setupVars();
+	}
+
+	protected function setupVars()
+	{
+		$this->post_types = array_keys($this->wordpress->get_option("elastic-posts_post_types_post_types"));
+		$this->index = $this->wordpress->get_option("elastic-posts_index_index");
+		$this->apiBase = "http://" . ENV . ".jhu.edu/api";
+
 		if (!$this->post_types) {
 			// @log and email devs
-			// die();
 		}
 
-		$this->wputils = new WordPressUtils();
-		$this->client = new \Elasticsearch\Client($this->getConfig());
+		if (!$this->index) {
+			// @log and email devs
+		}
 	}
 
 	/**
@@ -30,11 +51,11 @@ class Elasticsearch
 	 */
 	protected function getConfig()
 	{
-		$box = get_option("elastic-posts_box_box");
+		$box = $this->wordpress->get_option("elastic-posts_box_box");
 		$secrets = Secret::get("qbox", $box);
 
 		return array(
-			"hosts" => array($secrets->connections->https->url),
+			"hosts" => array($secrets->url . ":80"),
 			"connectionParams" => array(
 				"auth" => array(
 					$secrets->username,
@@ -54,7 +75,8 @@ class Elasticsearch
 	public function putOne($id)
 	{
 		$id = $this->getTrueId($id);
-		$post = $this->wputils->getPost($id);
+
+		$post = $this->httpEngine->get("{$this->apiBase}/{$id}", array("clear_cache" => true))->getBody()->data;
 
 		// make sure an unpublished post doesn't sneak
 		// through (for example, when a post is restored
@@ -66,14 +88,12 @@ class Elasticsearch
 
 		// this post type shoud not be saved
 		if (!in_array($post->post_type, $this->post_types)) return false;
-
-		$cleaner = $this->getCleaner($post->post_type);
 		
 		$params = array(
 			"index" => $this->index,
 			"type" => $post->post_type,
 			"id" => $id,
-			"body" => $cleaner->clean($post)
+			"body" => $this->removeUselessWpStuff($post)
 		);
 
 		return $this->client->index($params);
@@ -98,7 +118,7 @@ class Elasticsearch
 	public function removeOne($id)
 	{
 		$id = $this->getTrueId($id);
-		$post = $this->wputils->getPost($id, false);
+		$post = $this->httpEngine->get("{$this->apiBase}/{$id}", array("clear_cache" => true))->getBody()->data;
 
 		// this post type was never saved
 		if (!in_array($post->post_type, $this->post_types)) return false;
@@ -132,39 +152,58 @@ class Elasticsearch
 	 * the WordPress database into elasticsearch
 	 * @return array $response Responses from elasticsearch
 	 */
-	public function putAll()
+	// public function putAll()
+	// {
+	// 	$responses = array();
+
+	// 	$posts = array();
+	// 	foreach ($this->post_types as $type) {
+	// 		$posts[$type] = $this->wputils->getPosts($type);
+	// 	}
+
+	// 	// put posts in elasticsearch
+	// 	foreach ($posts as $type => $posts) {
+
+	// 		$cleaner = $this->getCleaner($type);
+
+	// 		foreach ($posts as $post) {
+	// 			$params = array(
+	// 				"index" => $this->index,
+	// 				"type" => $type,
+	// 				"id" => $post->ID,
+	// 				"body" => $cleaner->clean($post)
+	// 			);
+
+	// 			$responses[] = $this->client->index($params);
+	// 		}
+	// 	}
+
+	// 	return $responses;
+	// }
+
+
+	protected function removeUselessWpStuff($post)
 	{
-		$responses = array();
+		unset($post->post_author);
+		unset($post->post_date_gmt);
+		unset($post->post_status);
+		unset($post->comment_status);
+		unset($post->ping_status);
+		unset($post->post_password);
+		unset($post->post_name);
+		unset($post->to_ping);
+		unset($post->pinged);
+		unset($post->post_modified_gmt);
+		unset($post->post_content_filtered);
+		unset($post->post_parent);
+		unset($post->guid);
+		unset($post->menu_order);
+		unset($post->post_type);
+		unset($post->post_mime_type);
+		unset($post->comment_count);
+		unset($post->filter);
 
-		$posts = array();
-		foreach ($this->post_types as $type) {
-			$posts[$type] = $this->wputils->getPosts($type);
-		}
-
-		// put posts in elasticsearch
-		foreach ($posts as $type => $posts) {
-
-			$cleaner = $this->getCleaner($type);
-
-			foreach ($posts as $post) {
-				$params = array(
-					"index" => $this->index,
-					"type" => $type,
-					"id" => $post->ID,
-					"body" => $cleaner->clean($post)
-				);
-
-				$responses[] = $this->client->index($params);
-			}
-		}
-
-		return $responses;
-	}
-
-	protected function getCleaner($type)
-	{
-		$cleanerName = "\\ElasticPosts\\PostTypes\\{$type}";
-		return new $cleanerName();
+		return $post;
 	}
 
 	/**
@@ -175,7 +214,7 @@ class Elasticsearch
 	 */
 	protected function getTrueId($id)
 	{
-		$revision = $this->wputils->wp_is_post_revision($id); // returns parent post ID if this post is a revision
+		$revision = $this->wordpress->wp_is_post_revision($id); // returns parent post ID if this post is a revision
 		return $revision === false ? $id : $revision;
 	}
 }
