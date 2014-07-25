b493fc8a477da01e02f67d5d7b1652d5ad686dc5
Jen Wachter <jenleighkelly@gmail.com>
2014-07-25 11:53:26 -0400

    Assigned fact_description to desctiption.

--- src/Cleaners/fact.php
+++ src/Cleaners/fact.php
@@ -7,7 +7,7 @@ class fact extends Base
     public function clean($post)
     {
         $post = parent::clean($post);
-        $post = $this->assignDescription($post, null);
+        $post = $this->assignDescription($post, "fact_description");
         $post = $this->assignSummary($post, null);
         $post = $this->removeUselessWpStuff($post);
         return $post;
