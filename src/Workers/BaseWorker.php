7099b9b9aa08d75e9ec13f1879fa9f2b3b653aaf
Jen Wachter <jenleighkelly@gmail.com>
2016-04-08 15:40:21 -0400

    Updated base worker to use origin server.

--- src/Workers/BaseWorker.php
+++ src/Workers/BaseWorker.php
@@ -57,7 +57,7 @@ abstract class BaseWorker
 
         $this->wordpress = isset($injection["wordpress"]) ? $injection["wordpress"] : new \WPUtilities\WordPressWrapper();
         $this->wordpress_query = isset($injection["wordpress_query"]) ? $injection["wordpress_query"] : new \WPUtilities\WPQueryWrapper();
-        $this->api = new \WPUtilities\API();
+        $this->api = new \WPUtilities\API(array(), true);
         $this->post_util = isset($injection["post_util"]) ? $injection["post_util"] : new \WPUtilities\Post();
 
         $this->setupVars();
